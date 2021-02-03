8c4060b9b17b34f5feefd9e85407e83e22629c2d
RubenSommer <kontakt@rubensommer.de>
2021-02-03 21:33:54 +0100

    added contact form, added websocket backend for the contact form

---
+++ src/backend-node/apps.js
@@ -0,0 +1,38 @@
+const WebSocket = require("ws");
+const nodemailer = require("nodemailer");
+
+const wss = new WebSocket.Server({ port: 4253 }); //create websocket server; port: 4253
+console.log("started websocket server bin to port 4253");
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    var objectmessage = JSON.parse(message);
+    console.log(objectmessage);
+    console.log("send mail");
+
+    var transporter = nodemailer.createTransport({
+        host: '',
+        port: ,
+        auth: {
+            type: 'custom',
+            user: '',
+            pass: ''
+        },
+    });
+
+    var mailOptions = {
+        from: objectmessage.email,
+        to: '',
+        subject: "Anfrage LUG Website",
+        text: objectmessage.text
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  });
+});
